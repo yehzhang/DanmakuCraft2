ddd55f098db7d50134fdde8233a5634f64b48af5
yehzhang <simonzhangintp@gmail.com>
2017-11-11 23:18:57 -0800

    Add buff module.

---
+++ src/buff.ts
@@ -0,0 +1,50 @@
+import {Entity} from './entity';
+
+export class BuffManager<E extends Entity> {
+  private activatedBuffs: Array<Buff<E>>;
+
+  constructor(private readonly entity: E) {
+    this.activatedBuffs = [];
+  }
+
+  activate(buff: Buff<E>) {
+    buff.initialize(this.entity);
+    this.activatedBuffs.push(buff);
+  }
+
+  tick() {
+    this.activatedBuffs = this.activatedBuffs.filter(buff => {
+      buff.tick();
+
+      if (buff.isExpired()) {
+        buff.reset(this.entity);
+        return false;
+      } else {
+        buff.update(this.entity);
+        return true;
+      }
+    });
+  }
+}
+
+export abstract class Buff<E extends Entity> {
+  private ticks: number;
+
+  constructor(private readonly lifetime: number) {
+    this.ticks = 0;
+  }
+
+  tick() {
+    this.ticks++;
+  }
+
+  isExpired() {
+    return this.ticks >= this.lifetime;
+  }
+
+  abstract initialize(entity: E);
+
+  abstract update(entity: E);
+
+  abstract reset(entity: E);
+}
