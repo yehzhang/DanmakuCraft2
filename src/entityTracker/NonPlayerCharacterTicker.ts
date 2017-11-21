5808f858d99731e54c1d57bbb93096be1f1015c7
yehzhang <simonzhangintp@gmail.com>
2017-11-20 20:23:01 -0800

    Add NonPlayerCharacterTicker.

---
+++ src/entityTracker/NonPlayerCharacterTicker.ts
@@ -0,0 +1,14 @@
+import TickListener from './TickListener';
+import {AnimatedEntity, NonPlayerCharacter} from '../entity/entity';
+import {Region} from '../entity/EntityManager';
+
+export default class NonPlayerCharacterTicker
+    implements TickListener<AnimatedEntity, NonPlayerCharacter> {
+  onTick(trackee: AnimatedEntity, currentRegions: Array<Region<NonPlayerCharacter>>): void {
+    for (let region of currentRegions) {
+      region.forEach(npc => {
+        npc.tick();
+      });
+    }
+  }
+}
