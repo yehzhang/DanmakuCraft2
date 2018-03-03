0101d277d34f29bcf56389e0d5445de3b15585a3
yehzhang <simonzhangintp@gmail.com>
2018-03-02 18:16:43 -0800

    Launcher Bugfix: match url that has query parameters.

--- scripts/bilibiliClientLauncher.user.js
+++ scripts/bilibiliClientLauncher.user.js
@@ -4,10 +4,10 @@
 // @version      0.2
 // @description  在 B 站播放器内玩《弹幕世界2》：https://www.bilibili.com/video/av19771370/。官网：https://danmakucraft.com
 // @author       yehzhang
-// @match        *://www.bilibili.com/video/av19771370/
+// @match        *://www.bilibili.com/video/av19771370/*
 // @grant        none
 // @run-at       document-idle
-// @license      AGPL-3.0+
+// @license      AGPL-3.0
 // ==/UserScript==
 
 (function () {
