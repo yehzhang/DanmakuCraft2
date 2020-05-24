487401a6d84314ad34fd21947ef900159be40774
yehzhang <simonzhangintp@gmail.com>
2020-05-23 17:29:52 -0700

    data migration

--- parse/migratePsqlJson.ts
+++ parse/migratePsqlJson.ts
@@ -1,78 +1,78 @@
-import { writeFile } from 'fs';
+import { mkdir, writeFile } from 'fs/promises';
+import * as _ from 'lodash';
 import path from 'path';
-import { promisify } from 'util';
 import commentDump from '../data/dump/comment.json';
 import commentUserDump from '../data/dump/comment_user.json';
 import externalUserDump from '../data/dump/external_user.json';
-import userDump from '../data/dump/user.json';
 
 async function outputCommentTable() {
-  const commentIdToUserId = new Map();
-  for (const { user_id: userId, comment_id: commentId } of commentUserDump as any) {
-    if (commentIdToUserId.has(commentId)) {
-      throw new TypeError(`Unexpected duplicate comment id: ${commentId}`);
-    }
-    if (userId === defaultUserId) {
-      // Do not migrate the default user -- just set `userId` to `null`.
-      continue;
-    }
-    commentIdToUserId.set(commentId, userId);
-  }
-
-  const commentTable = {
-    results: (commentDump as any).map(
-      ({ id, text, color, size, createdAt, coordinateX: x, coordinateY: y, chromatic }: any) => ({
-        text,
-        color,
-        size,
-        createdAt,
-        x,
-        y,
-        chromatic,
-        userId: commentIdToUserId.get(id),
-      })
-    ),
-  };
-
-  const outputPath = path.join(outputDirectory, 'Comment.json');
-  await promisify(writeFile)(outputPath, JSON.stringify(commentTable));
+  const table = (commentDump as any).map(
+    ({ id, text, color, size, createdAt, coordinateX, coordinateY, chromatic }: any) => ({
+      objectId: id.toString(),
+      text,
+      color: chromatic ? undefined : color,
+      size,
+      createdAt,
+      x: coordinateX,
+      y: coordinateY,
+      type: chromatic ? 'chromatic' : 'plain',
+    })
+  );
+  await outputTable('Comment', table, 4);
 }
 
-async function outputUserTable() {
-  const additionalUserData = new Map();
-  for (const { origin, correspondsTo, externalId } of externalUserDump as any) {
-    if (additionalUserData.has(correspondsTo)) {
+async function outputBilibiliUserCommentTable() {
+  const userIdToBilibiliUserId = new Map();
+  for (const { correspondsTo, externalId } of externalUserDump as any) {
+    if (userIdToBilibiliUserId.has(correspondsTo)) {
       throw new TypeError(
         `Unexpected multiple external users corresponding to the same user id: ${correspondsTo}`
       );
     }
-    additionalUserData.set(correspondsTo, {
-      originDomain: origin,
-      externalId,
+    userIdToBilibiliUserId.set(correspondsTo, externalId);
+  }
+
+  const table = [];
+  const defaultUserId = 1;
+  for (const { user_id: userId, comment_id: commentId } of commentUserDump as any) {
+    if (userId === defaultUserId) {
+      // Do not migrate the default user -- just set `userId` to `null`.
+      continue;
+    }
+
+    const bilibiliUserId = userIdToBilibiliUserId.get(userId);
+    if (!bilibiliUserId) {
+      continue;
+    }
+
+    table.push({
+      bilibiliUserId,
+      commentId: commentId.toString(),
     });
   }
 
-  const userTable = {
-    results: (userDump as any)
-      // Do not migrate the default user -- just set `user` to `null`.
-      .filter(({ id }: any) => id !== defaultUserId)
-      .map(({ id, createdAt }: any) => ({
-        objectId: id,
-        createdAt,
-        ...additionalUserData.get(id),
-      })),
-  };
-
-  const outputPath = path.join(outputDirectory, 'User.json');
-  await promisify(writeFile)(outputPath, JSON.stringify(userTable));
+  await outputTable('BilibiliUserComment', table);
 }
-
-const defaultUserId = 1;
 
 const outputDirectory = '../build/database_import/';
 
+async function outputTable(tableName: string, table: object[], partitions = 1) {
+  const chunkSize = Math.ceil(table.length / partitions);
+  await Promise.all(
+    _.chunk(table, chunkSize).map((partition, partitionIndex) => {
+      const outputPath = path.join(outputDirectory, `${tableName}_${partitionIndex}.json`);
+
+      const printedRows = partition.map((row) => JSON.stringify(row)).join(',\n');
+      const printedTable = `{"results": [\n${printedRows}\n]}\n`;
+
+      return writeFile(outputPath, printedTable);
+    })
+  );
+}
+
 async function main() {
-  await Promise.all([outputUserTable(), outputCommentTable()]);
+  await mkdir(outputDirectory, { recursive: true });
+  await Promise.all([outputCommentTable(), outputBilibiliUserCommentTable()]);
 }
 
 main();
