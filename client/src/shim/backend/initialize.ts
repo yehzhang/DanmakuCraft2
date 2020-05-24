f010ad9ab7839a4f8a7ee564231c7cd186befde8
yehzhang <simonzhangintp@gmail.com>
2020-05-23 22:33:00 -0700

    client dev env

--- client/src/shim/backend/initialize.ts
+++ client/src/shim/backend/initialize.ts
@@ -1,10 +1,15 @@
 import Parse from 'parse';
-import applicationId from '../../../../parse/config/prod/applicationId.json';
-import serverUrl from '../../../../parse/config/prod/serverUrl.json';
+import devApplicationId from '../../../../parse/config/dev/applicationId.json';
+import devServerUrl from '../../../../parse/config/dev/serverUrl.json';
+import prodApplicationId from '../../../../parse/config/prod/applicationId.json';
+import prodServerUrl from '../../../../parse/config/prod/serverUrl.json';
 
 function initialize() {
-  Parse.initialize(applicationId, '5Zfjq6zZHrIDR8scvgdebjqTMdbKVDWjzK695Ycm');
-  Parse.serverURL = serverUrl;
+  const javaScriptKey = __DEV__
+    ? 'z6FeA6kRsDJKvLXMzTMwbWRB07Ioxa3hmFVQyRFU'
+    : '5Zfjq6zZHrIDR8scvgdebjqTMdbKVDWjzK695Ycm';
+  Parse.initialize(__DEV__ ? devApplicationId : prodApplicationId, javaScriptKey);
+  Parse.serverURL = __DEV__ ? devServerUrl : prodServerUrl;
 }
 
 export default initialize;
