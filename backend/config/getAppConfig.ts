4a79f9bbfd4b2ccc6c9edd1663e6e44d1462393a
yehzhang <simonzhangintp@gmail.com>
2020-05-30 19:32:00 -0700

    refactor

--- backend/config/getAppConfig.ts
+++ backend/config/getAppConfig.ts
@@ -1,8 +1,6 @@
 import devApplicationId from './dev/applicationId.json';
-import devMasterKey from './dev/masterKey.json';
 import devServerUrl from './dev/serverUrl.json';
 import prodApplicationId from './prod/applicationId.json';
-import prodMasterKey from './prod/masterKey.json';
 import prodServerUrl from './prod/serverUrl.json';
 
 function getAppConfig(env: 'prod' | 'dev'): AppConfig {
@@ -10,13 +8,11 @@ function getAppConfig(env: 'prod' | 'dev'): AppConfig {
     case 'prod':
       return {
         applicationId: prodApplicationId,
-        masterKey: prodMasterKey,
         serverUrl: prodServerUrl,
       };
     case 'dev':
       return {
         applicationId: devApplicationId,
-        masterKey: devMasterKey,
         serverUrl: devServerUrl,
       };
   }
@@ -24,7 +20,6 @@ function getAppConfig(env: 'prod' | 'dev'): AppConfig {
 
 interface AppConfig {
   readonly applicationId: string;
-  readonly masterKey: string;
   readonly serverUrl: string;
 }
 
--- backend/script/internal/appConfig.ts
+++ backend/script/internal/appConfig.ts
@@ -1,13 +1,21 @@
+import devMasterKey from '../../config/dev/masterKey.json';
 import getAppConfig from '../../config/getAppConfig';
+import prodMasterKey from '../../config/prod/masterKey.json';
 
 const appConfig = (() => {
   if (process.env.ENV === 'prod') {
     console.log('Using prod environment');
-    return getAppConfig('prod');
+    return {
+      ...getAppConfig('prod'),
+      masterKey: prodMasterKey,
+    };
   }
   if (process.env.ENV === 'dev') {
     console.log('Using dev environment');
-    return getAppConfig('dev');
+    return {
+      ...getAppConfig('dev'),
+      masterKey: devMasterKey,
+    };
   }
   throw new TypeError('Expected environment dev or prod');
 })();
