5b5fcdd4a9ccac8b62967b66cded3a6ab7cc5566
yehzhang <simonzhangintp@gmail.com>
2020-06-01 15:49:09 -0700

    track Bilibili user setup for debugging

--- client/src/shim/bilibili/setUpUser.ts
+++ client/src/shim/bilibili/setUpUser.ts
@@ -1,4 +1,5 @@
 import Cookies from 'js-cookie';
+import { track } from 'mixpanel-browser';
 import { addLoadingTask, LoadingResult } from '../../component/Loading';
 import { Store } from '../redux';
 
@@ -6,12 +7,19 @@ function setUpUser(store: Store): LoadingResult {
   addLoadingTask(() => {
     const signedOutElement = document.querySelector('.nav-user-center .user-con.logout');
     if (signedOutElement) {
+      trackUserSignInState('signed out');
       return 'bilibiliNotSignedIn';
     }
 
     const userId = Cookies.get('DedeUserID') || null;
+    trackUserSignInState(userId ? 'signed in' : 'unknown');
+
     store.dispatch({ type: '[shim/bilibili] detected maybe signed-in user', userId });
   });
+}
+
+function trackUserSignInState(type: 'signed in' | 'signed out' | 'unknown') {
+  track('Bilibili user setup', { type });
 }
 
 export default setUpUser;
