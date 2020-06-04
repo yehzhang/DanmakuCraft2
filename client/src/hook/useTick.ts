e7e51b539ab2e8d2c3d468c23c0fc9dab3685132
yehzhang <simonzhangintp@gmail.com>
2020-06-04 15:48:08 -0700

    smooth lag compensation

--- client/src/hook/useTick.ts
+++ client/src/hook/useTick.ts
@@ -1,4 +1,4 @@
-import { useLayoutEffect, useRef } from 'react';
+import { RefObject, useLayoutEffect, useRef } from 'react';
 import application from '../shim/pixi/application';
 
 function useTick(callback: TickCallback | null) {
@@ -6,19 +6,36 @@ function useTick(callback: TickCallback | null) {
   callbackRef.current = callback;
 
   useLayoutEffect(() => {
-    const tick = () => {
-      if (callbackRef.current) {
-        callbackRef.current(application.ticker.deltaMS);
-      }
-    };
-
-    application.ticker.add(tick);
+    listenerRefs.add(callbackRef);
     return () => {
-      application.ticker.remove(tick);
+      listenerRefs.delete(callbackRef);
     };
   }, []);
 }
 
 export type TickCallback = (deltaMs: number) => void;
+
+function tick() {
+  const lastPerceivedMs = perceivedMs;
+
+  realMs += application.ticker.elapsedMS;
+  perceivedMs = Math.max(perceivedMs + 1000 / 60, realMs - 500);
+
+  const lagMs = realMs - perceivedMs;
+  perceivedMs += Math.min(lagMs, 2, Math.max(lagMs * 0.1, 0.1));
+
+  const deltaMs = perceivedMs - lastPerceivedMs;
+  for (const listenerRef of listenerRefs) {
+    if (listenerRef.current) {
+      listenerRef.current(deltaMs);
+    }
+  }
+}
+
+let realMs = 0;
+let perceivedMs = 0;
+const listenerRefs = new Set<RefObject<TickCallback>>();
+
+application.ticker.add(tick);
 
 export default useTick;
