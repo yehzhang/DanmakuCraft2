62986a26db6e23bbe6204afe17a58f9145b6aaa8
yehzhang <simonzhangintp@gmail.com>
2020-06-06 20:09:53 -0700

    refactor

--- client/src/shim/backend/initializeBackend.ts
+++ client/src/shim/backend/initializeBackend.ts
@@ -2,13 +2,14 @@ import Parse from 'parse';
 import getAppConfig from '../../../../backend/config/getAppConfig';
 
 function initializeBackend() {
-  const javaScriptKey = __DEV__
-    ? 'z6FeA6kRsDJKvLXMzTMwbWRB07Ioxa3hmFVQyRFU'
-    : '5Zfjq6zZHrIDR8scvgdebjqTMdbKVDWjzK695Ycm';
-  Parse.initialize(appConfig.applicationId, javaScriptKey);
-  Parse.serverURL = appConfig.serverUrl;
+  Parse.initialize(applicationId, javaScriptKey);
+  Parse.serverURL = serverUrl;
 }
 
-export const appConfig = getAppConfig(__DEV__ ? 'dev' : 'prod');
+export const javaScriptKey = __DEV__
+  ? 'z6FeA6kRsDJKvLXMzTMwbWRB07Ioxa3hmFVQyRFU'
+  : '5Zfjq6zZHrIDR8scvgdebjqTMdbKVDWjzK695Ycm';
+
+export const { applicationId, serverUrl } = getAppConfig(__DEV__ ? 'dev' : 'prod');
 
 export default initializeBackend;
