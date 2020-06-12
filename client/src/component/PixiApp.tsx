35383b0cb7b4f128b025d576861222face683813
yehzhang <simonzhangintp@gmail.com>
2020-06-12 13:05:05 -0700

    refactor

--- client/src/component/PixiApp.tsx
+++ client/src/component/PixiApp.tsx
@@ -1,14 +1,45 @@
 import * as React from 'react';
+import { black } from '../data/color';
+import BackgroundMusic from './BackgroundMusic';
+import Camera from './Camera';
+import Chest from './Chest';
+import CommentInputPreview from './CommentInputPreview';
 import ConsoleDisplay from './ConsoleDisplay';
-import MainView from './MainView';
-import OpeningView from './OpeningView';
+import CulledComments from './CulledComments';
+import CulledSigns from './CulledSigns';
+import EnvironmentAwareFullscreenColor from './EnvironmentAwareFullscreenColor';
+import FullscreenColor from './FullscreenColor';
+import MainViewFadeIn from './MainViewFadeIn';
+import Opening from './Opening';
 import PixiCanvas from './PixiCanvas';
+import SpeechBubble from './SpeechBubble';
+import Ticker from './Ticker';
+import TinyTelevision from './TinyTelevision';
+import Tutorial from './Tutorial';
+import View from './View';
 
 function PixiApp() {
   return (
     <PixiCanvas>
-      <OpeningView />
-      <MainView />
+      <View name="opening">
+        <FullscreenColor color={black} />
+        <Opening />
+      </View>
+      <View name="main">
+        <Ticker />
+        <BackgroundMusic />
+        <Tutorial />
+        <EnvironmentAwareFullscreenColor />
+        <Camera>
+          <CulledSigns />
+          <Chest />
+          <TinyTelevision />
+          <CommentInputPreview />
+          <CulledComments />
+          <SpeechBubble />
+        </Camera>
+        <MainViewFadeIn />
+      </View>
       {__DEV__ && <ConsoleDisplay />}
     </PixiCanvas>
   );
